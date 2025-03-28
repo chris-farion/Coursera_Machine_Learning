c0996cfdc99553873cf6b0920f275a46b20c493e
Christopher Farion <christopher.farion@gmail.com>
2025-03-28 12:37:05 -0700

    Vectorizing gradient descent algorithm. Beforehand it was a for loop.

--- ex1-octave/gradientDescentMulti.m
+++ ex1-octave/gradientDescentMulti.m
@@ -6,11 +6,8 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-num_of_features = length(theta);
-inners = zeros(num_of_features,1);
 
 for iter = 1:num_iters
-
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
     %               theta.
@@ -18,17 +15,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-    for i = 1:num_of_features
-      inners(i) = (alpha/m)*((X*theta-y)'* X(:,i));
-    endfor
-    theta = theta-inners;
-
+    theta = theta - (alpha/m)*(X'*(X*theta-y));
     % ============================================================
-
     % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
-
 end
 
 end
