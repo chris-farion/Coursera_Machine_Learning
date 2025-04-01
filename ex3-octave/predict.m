2837c6f1436e6f7c7d64f974acabaf8e93605d04
Christopher Farion <christopher.farion@gmail.com>
2025-04-01 15:40:31 -0700

    Implemented Neural Network Feedforward Propagation and prediction

--- ex3-octave/predict.m
+++ ex3-octave/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,15 +21,20 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% Add bias unit to Training Examples
+X = [ones(m, 1) X];
 
+% Perform first layer calculation
+first_layer = sigmoid(X * Theta1');
 
+% Add bias unit to first layer
+first_layer = [ones(size(first_layer,1),1) first_layer];
 
+%perform second/output layer calculation
+second_layer = sigmoid(first_layer * Theta2');
 
-
-
-
+% Set p to index of guess
+[value, p] = max(second_layer, [], 2);
 
 % =========================================================================
-
-
 end
