73a17ea0acbb4a00ce02e9a22a7aa5dde2449a90
Christopher Farion <christopher.farion@gmail.com>
2025-04-22 12:59:37 -0700

    Added function that counts the number of keywords in a given email

--- ex6-octave/processEmail.m
+++ ex6-octave/processEmail.m
@@ -97,11 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-masterList = size(vocabList)(1);
+number_of_words = length(vocabList);
 
-for i=1:masterList
+for i=1:number_of_words
   if strcmp(str,vocabList(i))==1
     word_indices = [word_indices i];
+    break;
   endif
 endfor
 
