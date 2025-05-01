bb4833eb3a48cfa82a318917e8a6b60aac12b0b0
Christopher Farion <christopher.farion@gmail.com>
2025-05-01 09:26:53 -0700

    Implemented code to choose epsilon/threshold

--- ex8-octave/selectThreshold.m
+++ ex8-octave/selectThreshold.m
@@ -12,27 +12,32 @@ F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
-    
+
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
     %               threshold and place the value in F1. The code at the
     %               end of the loop will compare the F1 score for this
     %               choice of epsilon and set it to be the best epsilon if
     %               it is better than the current choice of epsilon.
-    %               
+    %
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+% precision = truePositive/(truePositive + falsePositive);
+% recall = truePositive/(truePositive + falseNegative);
 
+%F1 = (2 * precision * recall)/(precision + recall);
 
+predictions = pval < epsilon;
 
+truePositive = sum(and(yval,predictions));
+falsePositive = sum(and(xor(yval,1),predictions));
+falseNegative = sum(and(yval,xor(predictions,1)));
 
+precision = truePositive/(truePositive + falsePositive);
+recall = truePositive/(truePositive + falseNegative);
 
-
-
-
-
-
+F1 = (2 * precision * recall)/(precision + recall);
 
 
     % =============================================================
